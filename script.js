687f317c7cfb086c67a68e29b5857d3097c812b7
Sin Senpai <st17172@gmail.com>
2025-02-07 20:50:11 +0530

    added playback video and teams page

--- script.js
+++ script.js
@@ -1,14 +1,27 @@
-let currentSlide = 0;
-const slides = document.querySelectorAll('.hero-image');
+// let currentSlide = 0;
+// const slides = document.querySelectorAll('.hero-image');
 
-function showSlide(index) {
-    slides.forEach((slide, i) => slide.classList.toggle('active', i === index));
-}
+// function showSlide(index) {
+//     slides.forEach((slide, i) => slide.classList.toggle('active', i === index));
+// }
 
-function moveSlide(step) {
-    currentSlide = (currentSlide + step + slides.length) % slides.length;
-    showSlide(currentSlide);
-}
+// function moveSlide(step) {
+//     currentSlide = (currentSlide + step + slides.length) % slides.length;
+//     showSlide(currentSlide);
+// }
 
-setInterval(() => moveSlide(1), 5000); // Auto rotate every 5 seconds
-showSlide(currentSlide);
+// setInterval(() => moveSlide(1), 5000); // Auto rotate every 5 seconds
+// showSlide(currentSlide);
+const video = document.querySelector(".hero-video video");
+
+video.addEventListener("mouseenter", () => {
+    video.play();  // Play the video when hovered
+    video.muted =  false;
+    video.volume  = 0.1;
+});
+
+video.addEventListener("mouseleave", () => {
+    video.pause();  // Pause the video when mouse leaves
+    video.muted =  true;
+
+});
